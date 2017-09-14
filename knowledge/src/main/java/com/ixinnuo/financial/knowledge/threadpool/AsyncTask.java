d3a2c74e21559ad3ddf2b9c3dde73a372a346f9c
aisino-liqq <liquanqiang@aisino.com>
2017-09-14 19:50:33 +0800

    补充多线程的结果使用

--- knowledge/src/main/java/com/ixinnuo/financial/knowledge/threadpool/AsyncTask.java
+++ knowledge/src/main/java/com/ixinnuo/financial/knowledge/threadpool/AsyncTask.java
@@ -1,25 +1,54 @@
 package com.ixinnuo.financial.knowledge.threadpool;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AsyncTask {
 
-    protected final Logger logger = LoggerFactory.getLogger(this.getClass());    
-    
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     // myTaskAsynPool即配置线程池的方法名，此处如果不写自定义线程池的方法名，会使用默认的线程池
-    //异常要抛出去
-    @Async("myTaskAsyncPool") 
-    public void doTask1(int i){  
+    // 异常要抛出去
+    @Async("myTaskAsyncPool")
+    public void doTask(int i) {
         try {
-            Thread.sleep(1000);//耗时1秒
-            logger.info("Task {} started.",i);
+            Thread.sleep(1000);// 耗时1秒
+            logger.info("Task {} started.", i);
         }
         catch (InterruptedException e) {
             e.printStackTrace();
-        }    
-    }   
+        }
+    }
+
+    @Async("myTaskAsyncPool")
+    public Future<String> doTask1(int i) throws InterruptedException {
+        logger.info("Task1 {} started.", i);
+        long start = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long end = System.currentTimeMillis();
+
+        logger.info("Task1 {} finished, time elapsed: {} ms.", i,end - start);
+
+        return new AsyncResult<>("Task1 "+i+" accomplished!");
+    }
+    
+    @Async("myTaskAsyncPool")
+    public CompletableFuture<String> doTask2(int i) throws InterruptedException {
+        logger.info("Task2 {} started.", i);
+        long start = System.currentTimeMillis();
+        Thread.sleep(1000);
+        long end = System.currentTimeMillis();
+        
+        logger.info("Task2 {} finished, time elapsed: {} ms.", i,end - start);
+        
+        return CompletableFuture.completedFuture("Task2 "+i+" accomplished!");
+    }
+
 }
--- knowledge/src/test/java/com/ixinnuo/financial/knowledge/threadpool/AsyncTaskTest.java
+++ knowledge/src/test/java/com/ixinnuo/financial/knowledge/threadpool/AsyncTaskTest.java
@@ -1,6 +1,11 @@
 package com.ixinnuo.financial.knowledge.threadpool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,16 +24,45 @@ public class AsyncTaskTest {
     @Autowired
     private AsyncTask asyncTask;
     
-    @Autowired
-    private Executor executor;
     
     @Test
-    public void test() throws InterruptedException{
+    public void testDoTask() throws InterruptedException{
         for (int i = 0; i < 30; i++) {  
-            asyncTask.doTask1(i);  
+            asyncTask.doTask(i);  
         } 
         //测试用，等待任务结束再终止
         System.out.println("all task finished");
         Thread.sleep(1000*15);
+    }
+    
+    @Test
+    public void testDoTask1() throws InterruptedException, ExecutionException{
+        List<Future> list = new ArrayList<Future>(); 
+        for (int i = 0; i < 30; i++) {  
+            Future<String> doTask1 = asyncTask.doTask1(i); 
+            list.add(doTask1);
+        } 
+        System.out.println("all task commited");
+        for(int i = 0;i<list.size();i++){
+            Future future = list.get(i);
+            System.out.println(future.get());
+        }
+        //测试用，等待任务结束再终止
+        System.out.println("all task finished");
+    }
+    @Test
+    public void testDoTask2() throws InterruptedException, ExecutionException{
+        List<Future> list = new ArrayList<Future>(); 
+        for (int i = 0; i < 30; i++) {  
+            Future<String> doTask2 = asyncTask.doTask2(i); 
+            list.add(doTask2);
+        } 
+        System.out.println("all task commited");
+        for(int i = 0;i<list.size();i++){
+            Future future = list.get(i);
+            System.out.println(future.get());
+        }
+        //测试用，等待任务结束再终止
+        System.out.println("all task finished");
     }
 }
