6d7d60c386208b522756aafb4f27875ff17376eb
aisino-liqq <liquanqiang@aisino.com>
2018-01-18 14:42:04 +0800

    schema学习

--- knowledge/src/main/java/com/ixinnuo/financial/knowledge/xml/XmlValidator.java
+++ knowledge/src/main/java/com/ixinnuo/financial/knowledge/xml/XmlValidator.java
@@ -13,15 +13,19 @@ public class XmlValidator {
 	public static final String W3C_XML_SCHEMA_NS_URI = "http://www.w3.org/2001/XMLSchema";
 
 	public static void main(String[] args) {
-		String xsdPath = "src/main/java/com/ixinnuo/financial/knowledge/xml/note.xsd";
-		String xmlPath = "src/main/java/com/ixinnuo/financial/knowledge/xml/note.xml";
-		validateXMLSchema(xsdPath, xmlPath);
+		String xsdPath = "src/main/java/com/ixinnuo/financial/knowledge/xml/my.xsd";
+		String xmlPath = "src/main/java/com/ixinnuo/financial/knowledge/xml/my.xml";
+		String result = validateXMLSchema(xsdPath, xmlPath);
+		System.out.println(result.equals("") == true ? "通过" : "未通过");
 	}
 
 	/**
 	 * 使用xsd验证xml文件
-	 * @param xsdPath xsd文件路径
-	 * @param xmlPath xml文件路径
+	 * 
+	 * @param xsdPath
+	 *            xsd文件路径
+	 * @param xmlPath
+	 *            xml文件路径
 	 * @return 错误信息，空字符串表示通过
 	 */
 	public static String validateXMLSchema(String xsdPath, String xmlPath) {
