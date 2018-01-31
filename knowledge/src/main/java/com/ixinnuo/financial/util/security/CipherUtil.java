85204c4985de9286207cc9f728c1b37447887e34
aisino-liqq <liquanqiang@aisino.com>
2018-01-31 12:34:19 +0800

    3des/ecb/zeropadding 加密

--- knowledge/src/main/java/com/ixinnuo/financial/util/security/CipherUtil.java
+++ knowledge/src/main/java/com/ixinnuo/financial/util/security/CipherUtil.java
@@ -5,6 +5,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
+import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -27,7 +28,7 @@ public class CipherUtil {
 	public static final String CHARSET_UTF8 = "UTF-8";
 	public static final String Algorithm_AES = "AES";
 	public static final String Algorithm_DES = "DES";
-	public static final String Algorithm_DESede = "DESede";// 也叫3DES/模式/填充
+	public static final String Algorithm_DESede = "DESede";// 也叫3DES/模式/填充，默认等同于DESede/ECB/PKCS5Padding
 	public static final String Algorithm_RSA = "RSA";
 	public static final String Algorithm_PBEWithMD5AndDES = "PBEWithMD5AndDES";
 
@@ -47,7 +48,12 @@ public class CipherUtil {
 		System.out.println("\n" + Algorithm_DESede + "加密串：" + encryptMsg2);
 		String decryptMsg2 = decryptMsg(key, Algorithm_DESede, encryptMsg2);
 		System.out.println("\n" + Algorithm_DESede + "解密后：" + decryptMsg2);
-
+		System.out.println("==============以下是3des/ecb/zeroPadding加解密的===================");
+		String deskey = "111111111111111111111111";
+		String encryptWithDESCEBZeroPading = encryptWithDESCEBZeroPading(deskey, "1234567890");
+		System.out.println("\n加密串" + encryptWithDESCEBZeroPading);
+		String decryptWithDESCEBZeroPading = decryptWithDESCEBZeroPading(deskey, encryptWithDESCEBZeroPading);
+		System.out.println("\n解密串" + decryptWithDESCEBZeroPading);
 	}
 
 	/**
@@ -156,6 +162,50 @@ public class CipherUtil {
 		}
 		return null;
 	}
+	
+	/**
+	 * 3des ecb 0填充，利用NoPading，自行补0
+	 * @param key 密钥串，必须为24长度字符串
+	 * @param msg 消息
+	 * @return Base64编码后的字符串
+	 */
+	public static String encryptWithDESCEBZeroPading(String key,String msg) {
+		try {
+			// 生成密钥
+			byte[] bytes = key.getBytes(CHARSET_UTF8);
+			System.out.println("密钥字节长度：" + bytes.length);
+			SecretKey deskey = new SecretKeySpec(bytes, "DESede");
+			// 加密工具
+			Cipher c1 = Cipher.getInstance("DESede/ECB/NoPadding");
+			// 加密
+			c1.init(Cipher.ENCRYPT_MODE, deskey);
+			byte[] msgBytes = msg.getBytes(CHARSET_UTF8);
+			System.out.println("原数据字节如下：");
+			for (int i = 0; i < msgBytes.length; i++) {
+				System.out.print(msgBytes[i] + "\t");
+			}
+			//FIXME 自行补位，达到8字节的倍数
+			int remainder = msgBytes.length % 8;
+			if(0 != remainder){
+				int oldLength = msgBytes.length;
+				//1.扩展自身长度
+				msgBytes = Arrays.copyOf(msgBytes, msgBytes.length + 8-remainder);
+				//2.填充扩展内容为0
+				Arrays.fill(msgBytes, oldLength, msgBytes.length, (byte)0);
+				
+			}
+			//FIXME 此处不能使用update,自行补位，
+			byte[] doFinal = c1.doFinal(msgBytes);
+			System.out.println("\n加密后的字节如下：");
+			for (int i = 0; i < doFinal.length; i++) {
+				System.out.print(doFinal[i] + "\t");
+			}
+			return Base64.getEncoder().encodeToString(doFinal);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	/**
 	 * 没有证书的情况下，使用密钥串解密
@@ -185,6 +235,50 @@ public class CipherUtil {
 			}
 			//FIXME 此处不能使用update
 			byte[] doFinal = instance.doFinal(encryptMsgBytes);
+			System.out.println("\n解密后的字节为如下：");
+			for (int i = 0; i < doFinal.length; i++) {
+				System.out.print(doFinal[i] + "\t");
+			}
+			return new String(doFinal, CHARSET_UTF8);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * 3des ecb 0填充，解密；利用NoPading，自行去除填充的0
+	 * @param key 密钥字符串，必须是24长度
+	 * @param encryptMsg base64的密文
+	 * @return
+	 */
+	public static String decryptWithDESCEBZeroPading(String key,String encryptMsg) {
+		try {
+			// 生成密钥
+			byte[] bytes = key.getBytes(CHARSET_UTF8);
+			System.out.println("密钥字节长度：" + bytes.length);
+			SecretKey deskey = new SecretKeySpec(bytes, "DESede");
+			//初始工具
+			Cipher instance = Cipher.getInstance("DESede/ECB/NoPadding");
+			// DECRYPT_MODE 解密模式
+			instance.init(Cipher.DECRYPT_MODE, deskey);
+			byte[] encryptMsgBytes = Base64.getDecoder().decode(encryptMsg);
+			System.out.println("待解密的字节如下：");
+			for (int i = 0; i < encryptMsgBytes.length; i++) {
+				System.out.print(encryptMsgBytes[i] + "\t");
+			}
+			//FIXME 此处不能使用update
+			byte[] doFinal = instance.doFinal(encryptMsgBytes);
+			//去除填充的0,倒数第一个不为0的位置，copy到另一个数组
+			int zeroIndex = 0;
+			for (int i = doFinal.length-1; i > 0; i--) {
+				if(doFinal[i] == (byte)0){
+					zeroIndex = i;
+				}else{
+					break;
+				}
+			}
+			doFinal = Arrays.copyOf(doFinal, zeroIndex);
 			System.out.println("\n解密后的字节为如下：");
 			for (int i = 0; i < doFinal.length; i++) {
 				System.out.print(doFinal[i] + "\t");
