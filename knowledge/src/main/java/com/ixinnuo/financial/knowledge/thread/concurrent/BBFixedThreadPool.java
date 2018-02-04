9d90929f7cb9bcb05c085344d238b66ea65326cd
quanqiang-li <2476056494@qq.com>
2018-02-04 21:19:18 +0800

    自定义线程池

--- knowledge/src/main/java/com/ixinnuo/financial/knowledge/thread/concurrent/BBFixedThreadPool.java
+++ knowledge/src/main/java/com/ixinnuo/financial/knowledge/thread/concurrent/BBFixedThreadPool.java
@@ -1,16 +1,20 @@
 package com.ixinnuo.financial.knowledge.thread.concurrent;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 自定义线程池
+ * 自定义线程池,
+ * LinkedBlockingQueue 无界阻塞队列，使用的节点，node实现
+ * ArrayBlockingQueue  有界阻塞队列，使用的数组，所以必须定长
  * 
  * @author 2476056494@qq.com
  *
@@ -26,15 +30,27 @@ public class BBFixedThreadPool {
 		long keepAliveTime = 0L;
 		//keepAliveTime 单位
 		TimeUnit unit = TimeUnit.SECONDS;
-		ThreadFactory handler = Executors.defaultThreadFactory();
-		BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(2);
+		//默认的线程工厂，创建线程
+		ThreadFactory threadFactory = Executors.defaultThreadFactory();
+		//阻塞队列
+		//BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(2);
+		BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(2);
+		//超过线程池的最大处理能力时，拒绝策略
+		RejectedExecutionHandler rejectedExeutionHandler = new RejectedExecutionHandler() {
+			
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+				System.out.println(System.currentTimeMillis() + executor.toString() + r.toString() + "..rejected...");
+				
+			}
+		};
 		// 【1】获取线程池，固定2个线程
-		ExecutorService fixedThreadPool = new ThreadPoolExecutor(corePoolSize , maximumPoolSize , keepAliveTime , unit ,
-				workQueue, handler);
-		// 【2】提交4个任务，观察任务的执行时间和线程name
-		for (int i = 0; i < 4; i++) {
+		ExecutorService executorService = new ThreadPoolExecutor(corePoolSize , maximumPoolSize , keepAliveTime , unit ,
+				workQueue, threadFactory,rejectedExeutionHandler );
+		// 【2】提交40个任务，观察任务的执行时间和线程name
+		for (int i = 0; i < 40; i++) {
 			Task task = new Task();
-			fixedThreadPool.submit(task);
+			executorService.submit(task);
 		}
 	}
 
